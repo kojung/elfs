6411e5480d85655d03872eb2610c84f459f197f9
Jung Ko <kojung@gmail.com>
2020-05-17 18:58:55 -0700

    refactor non-template functions into base class

--- firmware/target-controller/Target.h
+++ firmware/target-controller/Target.h
@@ -19,91 +19,33 @@
 #pragma once
 
 #include <Arduino.h>
-#include "FastLED.h"
-
-#define TARGET_NUM_LEDS                   (16)
-#define TARGET_DEFAULT_SENSOR_THRESHOLD  (400)
-#define TARGET_DEFAULT_TIMER_INTERVAL   (1000)
-#define TARGET_DEFAULT_RING_BRIGHTNESS   (128)
+#include "TargetBase.h"
 
 typedef uint8_t pin_t;
 
-typedef enum target_mode_e {
-    TARGET_ENABLED = 0,        // target enabled
-    TARGET_TIMED,              // target enabled for a limited time
-    TARGET_DISABLED            // target disabled
-} target_mode_t;
-
 template<pin_t LED, pin_t LDR, pin_t TRIGGER>
-class Target {
+class Target : public TargetBase {
  public:
-    /** constructor
-    */
+    /** Constructor */
     Target();
 
-    /** Set target mode
-    * @param mode Target mode
-    */
-    void set_mode(target_mode_t mode);
+    /** Destructor */
+    ~Target();
 
     /** Update target
     * Read the sensor and update internal state. Call this function
     * inside the Arduino main loop as fast as possible.
     * @return True if hit state changed
     */
-    bool update();
-
-    /** Return target state
-    * @return True if target has been hit, False otherwise
-    */
-    bool get_hit_state();
-
-    /** Accessors for sensor threshold */
-    void set_sensor_threshold(int sensor_threshold);
-    int get_sensor_threshold();
-
-    /** Accessors for timer interval */
-    void set_timer_interval(unsigned long timer_interval);
-    unsigned long get_timer_interval();
-
-    /** Accessors for ring brightness */
-    void set_ring_brightness(int brightness);
-    int get_ring_brightness();
+    bool update() override;
 
  private:
-    void set_color(uint8_t count, CRGB color);
-
-    // FastLED controller
-    CLEDController* controller_;
-
     // provide feedback for hit
     void hit_feedback();
-
-    // Current target mode
-    target_mode_t mode_;
-
-    // Flag indicating if target has been hit or not
-    bool hit_state_;
-
-    // Adjust sensor sensitivity
-    int sensor_threshold_;
-
-    // Adjust ring brightness
-    int ring_brightness_;
-
-    // Array holding ring colors
-    CRGB ring_[TARGET_NUM_LEDS];
-
-    // Counters used in TIMED mode
-    uint8_t led_counter_;              // count LEDs
-    unsigned long last_update_time_;   // keep track of last time
-    unsigned long timer_interval_;     // how often to update the LED counter
 };
 
 template<pin_t LED, pin_t LDR, pin_t TRIGGER>
-Target<LED, LDR, TRIGGER>::Target() :
-    sensor_threshold_(TARGET_DEFAULT_SENSOR_THRESHOLD),
-    timer_interval_(TARGET_DEFAULT_TIMER_INTERVAL) {
+Target<LED, LDR, TRIGGER>::Target() {
     // configure pins
     pinMode(LED,     OUTPUT);
     pinMode(LDR,     INPUT);  // analog
@@ -117,17 +59,8 @@ Target<LED, LDR, TRIGGER>::Target() :
 }
 
 template<pin_t LED, pin_t LDR, pin_t TRIGGER>
-void Target<LED, LDR, TRIGGER>::set_mode(target_mode_t mode) {
-    mode_ = mode;
-    if (mode_ == TARGET_ENABLED) {
-        set_color(TARGET_NUM_LEDS, CRGB::Green);
-    } else if (mode_ == TARGET_TIMED) {
-        led_counter_ = TARGET_NUM_LEDS;
-        set_color(led_counter_, CRGB::Green);
-        last_update_time_ = millis();
-    } else if (mode_ == TARGET_DISABLED) {
-        set_color(TARGET_NUM_LEDS, CRGB::Black);
-    }
+Target<LED, LDR, TRIGGER>::~Target() {
+    // nothing to destroy
 }
 
 template<pin_t LED, pin_t LDR, pin_t TRIGGER>
@@ -168,50 +101,4 @@ void Target<LED, LDR, TRIGGER>::hit_feedback() {
             digitalWrite(TRIGGER, LOW);
         }
     }
-}
-
-template<pin_t LED, pin_t LDR, pin_t TRIGGER>
-bool Target<LED, LDR, TRIGGER>::get_hit_state() {
-    return hit_state_;
-}
-
-template<pin_t LED, pin_t LDR, pin_t TRIGGER>
-void Target<LED, LDR, TRIGGER>::set_color(uint8_t count, CRGB color) {
-    for (uint8_t i=0; i<count; i++) {
-        ring_[i] = color;
-    }
-    for (uint8_t i=count; i<TARGET_NUM_LEDS; i++) {
-        ring_[i] = CRGB::Black;
-    }
-    controller_->showLeds(ring_brightness_);
-}
-
-template<pin_t LED, pin_t LDR, pin_t TRIGGER>
-void Target<LED, LDR, TRIGGER>::set_sensor_threshold(int sensor_threshold) {
-    sensor_threshold_ = sensor_threshold;
-}
-
-template<pin_t LED, pin_t LDR, pin_t TRIGGER>
-int Target<LED, LDR, TRIGGER>::get_sensor_threshold() {
-    return sensor_threshold_;
-}
-
-template<pin_t LED, pin_t LDR, pin_t TRIGGER>
-void Target<LED, LDR, TRIGGER>::set_timer_interval(unsigned long timer_interval) {
-    timer_interval_ = timer_interval;
-}
-
-template<pin_t LED, pin_t LDR, pin_t TRIGGER>
-unsigned long Target<LED, LDR, TRIGGER>::get_timer_interval() {
-    return timer_interval_;
-}
-
-template<pin_t LED, pin_t LDR, pin_t TRIGGER>
-void Target<LED, LDR, TRIGGER>::set_ring_brightness(int ring_brightness) {
-    ring_brightness_ = ring_brightness;
-}
-
-template<pin_t LED, pin_t LDR, pin_t TRIGGER>
-int Target<LED, LDR, TRIGGER>::get_ring_brightness() {
-    return ring_brightness_;
 }
---
+++ firmware/target-controller/TargetBase.h
@@ -0,0 +1,98 @@
+/*
+* Enhanced Laser Firing System - ELFS
+* Copyright (C) 2020 Jung Ko <kojung@gmail.com>
+* 
+* This program is free software: you can redistribute it and/or modify
+* it under the terms of the GNU General Public License as published by
+* the Free Software Foundation, either version 3 of the License, or
+* (at your option) any later version.
+* 
+* This program is distributed in the hope that it will be useful,
+* but WITHOUT ANY WARRANTY; without even the implied warranty of
+* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+* GNU General Public License for more details.
+* 
+* You should have received a copy of the GNU General Public License
+* along with this program. If not, see <https://www.gnu.org/licenses/>.
+*/
+
+#pragma once
+
+#include <Arduino.h>
+#include "FastLED.h"
+
+#define TARGET_NUM_LEDS                   (16)
+#define TARGET_DEFAULT_SENSOR_THRESHOLD  (400)
+#define TARGET_DEFAULT_TIMER_INTERVAL   (1000)
+#define TARGET_DEFAULT_RING_BRIGHTNESS   (128)
+
+typedef enum target_mode_e {
+    TARGET_ENABLED = 0,        // target enabled
+    TARGET_TIMED,              // target enabled for a limited time
+    TARGET_DISABLED            // target disabled
+} target_mode_t;
+
+class TargetBase {
+ public:
+    /** Constructor */
+    TargetBase();
+
+    /** Pure virtual destructor */
+    virtual ~TargetBase() = 0;
+
+    /** Update target
+    * Read the sensor and update internal state. Call this function
+    * inside the Arduino main loop as fast as possible.
+    * @return True if hit state changed
+    */
+    virtual bool update();
+
+    /** Set target mode
+    * @param mode Target mode
+    */
+    void set_mode(target_mode_t mode);
+
+    /** Return target state
+    * @return True if target has been hit, False otherwise
+    */
+    bool get_hit_state();
+
+    /** Accessors for sensor threshold */
+    void set_sensor_threshold(int sensor_threshold);
+    int get_sensor_threshold();
+
+    /** Accessors for timer interval */
+    void set_timer_interval(unsigned long timer_interval);
+    unsigned long get_timer_interval();
+
+    /** Accessors for ring brightness */
+    void set_ring_brightness(int brightness);
+    int get_ring_brightness();
+
+ protected:
+    // update the ring led
+    void set_color(uint8_t count, CRGB color);
+
+    // FastLED controller
+    CLEDController* controller_;
+
+    // Current target mode
+    target_mode_t mode_;
+
+    // Flag indicating if target has been hit or not
+    bool hit_state_;
+
+    // Adjust sensor sensitivity
+    int sensor_threshold_;
+
+    // Adjust ring brightness
+    int ring_brightness_;
+
+    // Array holding ring colors
+    CRGB ring_[TARGET_NUM_LEDS];
+
+    // Counters used in TIMED mode
+    uint8_t led_counter_;              // count LEDs
+    unsigned long last_update_time_;   // keep track of last time
+    unsigned long timer_interval_;     // how often to update the LED counter
+};
