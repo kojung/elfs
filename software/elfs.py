4c61e87b12bd33d83d069eb418a233c2842ba35c
Jung Ko <kojung@gmail.com>
2020-06-02 19:05:24 -0700

    stash

--- software/elfs.py
+++ software/elfs.py
@@ -1,31 +1,48 @@
 from flask import Flask, render_template, Response
-from queue import Queue
+import Queue
 import time
+import threading
+import atexit
 
 from controller import Controller
 
 app = Flask("ELFS")
 
 # instantiate controller
-ctrl = Controller()
+SERIAL         = '/dev/ttyUSB0'
+BAUDRATE       = 9600
+NUM_OF_TARGETS = 4
+ctrl = Controller(SERIAL, BAUDRATE)
 
 # use this queue to communicate with the controller
-queue = Queue()
+queue  = Queue.Queue()
+reader = threading.Thread(target=ctrl.reader, args=[queue])
+reader.start()
 
+def shutdown_controller():
+    """shutdown the controller gracefully"""
+    print("Shutting down controller")
+    ctrl.terminate = True
+    reader.join()
+    NUM_OF_TARGETS = 4
+    for i in range(NUM_OF_TARGETS):
+        ctrl.set_target(i, "DISABLED")
+    
 def get_message():
-    '''this could be any function that blocks until data is ready'''
-    time.sleep(1.0)
-    s = time.ctime(time.time())
+    """this could be any function that blocks until data is ready"""
+    return queue.get()
     return s
 
 @app.route('/')
 def index():
     return render_template('index.html')
 
-@app.route('/elfs')
+@app.route('/controller')
 def controller():
     def eventStream():
         while True:
             # wait for source data to be available, then push it
             yield 'data: {}\n\n'.format(get_message())
     return Response(eventStream(), mimetype="text/event-stream")
+
+atexit.register(xxxhutdown_controller)
