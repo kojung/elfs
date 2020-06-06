1d40522ff6a6cb64e8b7a10bdd87bdad2bcb141f
Jung Ko <kojung@gmail.com>
2020-06-06 11:14:12 -0700

    implemented stop timer

--- software/elfs.py
+++ software/elfs.py
@@ -2,10 +2,11 @@
 
 from flask import Flask, render_template, Response
 from flask_bootstrap import Bootstrap, StaticCDN
-import queue
+from queue import Empty, Queue
 import time
 import threading
 import atexit
+import re
 
 from controller import Controller
 
@@ -21,30 +22,51 @@ BAUDRATE       = 9600
 NUM_OF_TARGETS = 4
 ctrl = Controller(SERIAL, BAUDRATE)
 
-# use this queue to communicate with the controller
-queue  = queue.Queue()
-reader = threading.Thread(target=ctrl.reader, args=[queue])
-reader.start()
+# singleton that keeps track of shared state
+state = {
+    'queue': Queue(),
+    'target': [{'score': 0, 'color':'lightgray'} for x in range(NUM_OF_TARGETS)],
+    'timer': 0,
+    'total_score': 0,
+    'mode': 'practice',
+    'started': False,
+    'timer_limit': 20,
+}
 
-queue.put("Hello")
-queue.put("World")
-for i in range(10):
-    queue.put(i)
+# start controller thread
+reader_tid = threading.Thread(target=ctrl.reader, args=[state['queue']])
+reader_tid.start()
+
+# start timer thread
+def timer_thread(state):
+    timer = 0
+    queue = state['queue']
+    while True:
+        while timer != state['timer_limit']:
+            time.sleep(1)
+            timer += 1
+            queue.put(f"TIMER {timer}")
+
+timer_tid = threading.Thread(target=timer_thread, args=[state])
+timer_tid.start()   
 
 def shutdown_controller():
    # """shutdown the controller gracefully"""
    print("INFO: Shutting down controller")
    ctrl.terminate = True
-   reader.join()
+   reader_tid.join()
    NUM_OF_TARGETS = 4
    for i in range(NUM_OF_TARGETS):
        ctrl.set_target(i, "DISABLED")
     
-def process_queue(queue):
+def process_queue(state):
     """this could be any function that blocks until data is ready"""
-    cmd = queue.get()
-    time.sleep(1.0)
-    return cmd
+    cmd = state['queue'].get()
+    timer_cmd = re.search(r'TIMER (\d+)', cmd)
+    if timer_cmd:
+        state['timer'] = int(timer_cmd.group(1))
+
+    return state['timer']
 
 @app.route('/')
 def index():
@@ -55,7 +77,7 @@ def controller():
     def eventStream():
         while True:
             # wait for source data to be available, then push it
-            cmd = process_queue(queue)
+            cmd = process_queue(state)
             yield f'data: {cmd}\n\n'
     return Response(eventStream(), mimetype="text/event-stream")
 
